e91b6a441863fd1efbac29dc7d2d4aec590eb832
Florian Bacher <bacherfl@gmail.com>
2015-06-11 14:30:50 +0200

    added JSONDoc documentation and API playground

--- src/main/java/sdicn/controller/ContentLocationController.java
+++ src/main/java/sdicn/controller/ContentLocationController.java
@@ -1,10 +1,11 @@
 package sdicn.controller;
 
+import org.jsondoc.core.annotation.Api;
+import org.jsondoc.core.annotation.ApiMethod;
+import org.jsondoc.core.annotation.ApiQueryParam;
+import org.jsondoc.core.annotation.ApiResponseObject;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import model.LocationInfo;
 import sdicn.StatisticsService;
 import sdicn.model.RequestInfo;
@@ -17,6 +18,8 @@ import java.util.Map;
 /**
  * Created by florian on 27.04.2015.
  */
+
+@Api(name = "Location services", description = "Methods for managing locations")
 @RestController
 public class ContentLocationController {
 
@@ -28,11 +31,15 @@ public class ContentLocationController {
 
     public ContentLocationController() {
         contentLocations = new HashMap<>();
+        contentSizes = new HashMap<>();
     }
 
+    @ApiMethod
     @RequestMapping(value = "/location/resolve", method = RequestMethod.GET)
-    public LocationInfo resolveContentLocation(@RequestParam(value = "contentName") String contentName,
-                                               @RequestParam(value = "client") String client) {
+    public @ApiResponseObject
+    @ResponseBody LocationInfo resolveContentLocation(
+            @ApiQueryParam(name = "contentName") @RequestParam(value = "contentName") String contentName,
+            @ApiQueryParam(name = "client") @RequestParam(value = "client") String client) {
         statisticsService.storeRequestInfo(new RequestInfo(client, contentName));
         if (contentLocations.containsKey(contentName)) {
             LocationInfo locationInfo = new LocationInfo();
@@ -42,10 +49,11 @@ public class ContentLocationController {
         else return null;
     }
 
+    @ApiMethod
     @RequestMapping(value = "/location/add", method = RequestMethod.POST)
-    public void addContentLocation(@RequestParam String contentName,
-                                   @RequestParam String contentLocation,
-                                   @RequestParam long size) {
+    public void addContentLocation(@ApiQueryParam(name = "contentName") @RequestParam String contentName,
+                                   @ApiQueryParam(name = "contentLocation") @RequestParam String contentLocation,
+                                   @ApiQueryParam(name= "size") @RequestParam long size) {
         if (!contentLocations.containsKey(contentName)) {
             contentLocations.put(contentName, new ArrayList<String>());
         }
